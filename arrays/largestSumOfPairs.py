31014c3c19175c91642c00e0c407448bff4cd8cc
Khanh Luc <itsmekhanh@gmail.com>
2015-05-25 15:21:32 -0700

    finished largestSumOfPairs

--- arrays/largestSumOfPairs.py
+++ arrays/largestSumOfPairs.py
@@ -3,7 +3,7 @@ def largestSumOfPairs(values):
         print "Input array contains less than 2 values"
         return False
 
-    largestPair = []
+    largestPair = [None]*2
     for value in values:
         if largestPair[1] is None:
             largestPair[1] = value
@@ -11,12 +11,14 @@ def largestSumOfPairs(values):
             largestPair[0] = largestPair[1]
             largestPair[1] = value
         elif largestPair[0] < value:
-            largest[0] = value
+            largestPair[0] = value
 
     return largestPair[0]+largestPair[1]
 
 if __name__ == '__main__':
-    print "Problem: Given an unsorted array, find the largest pair sum in it\n"
-    print "Testing list: [12,40,10,6,34]\n"
-    print "Sum is " + largestSumOfPairs([12,40,10,6,34])+"\n"
+    print "Problem: Given an unsorted array, find the largest pair sum in it"
+    print "Testing list: [12,40,10,6,34]"
+    print "Sum is ", largestSumOfPairs([12,40,10,6,34])
+    print "Testing list: [40,30,1,2,3,4]"
+    print "Sum is ", largestSumOfPairs([40,30,1,2,3,4])
 
