f97650b4aca0839099dbed457f4b684a98f74916
Khanh Luc <khanh@classroom.tv>
2015-05-27 16:42:40 -0700

    added maxProfit

---
+++ arrays/maxProfit.py
@@ -0,0 +1,10 @@
+def maxProfit(prices):
+
+if __name__ == "__main__":
+    print """
+In a daily share trading, a buyer buys shares in the morning and sells it on same day.
+If the trader is allowed to make at most 2 transactions in a day,
+where as second transaction can only start after first one is complete (Sell->buy->sell->buy).
+Given stock prices throughout day, find out maximum profit that a share trader could have made.
+"""
+    print "Testing maxProfit()"
